57861a1c7ef36096dd6c79f8daef3494b10355c6
agacia1805 <agacia1805@gmail.com>
2024-01-15 20:39:59 +0000

    (feat) create footer component

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,33 @@
+import {
+  ButtonGroup,
+  Container,
+  IconButton,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Link } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export const Footer = () => (
+  <Container as='footer' role='contentinfo' py={{ base: '12', md: '16' }}>
+    <Stack spacing={{ base: '4', md: '5' }}>
+      <Stack justify='space-between' direction='row' align='center'>
+        <Text fontSize='sm' fontWeight='semibold' color='fg.subtle'>
+          &copy; {new Date().getFullYear()} Pinehead Coder
+        </Text>
+        <ButtonGroup variant='tertiary'>
+          <Link
+            as={NextLink}
+            href='https://www.linkedin.com/in/agatadopierala/'
+          >
+            <IconButton aria-label='LinkedIn' icon={<FaLinkedin />} />
+          </Link>
+          <Link as={NextLink} href='https://github.com/agacia1805'>
+            <IconButton aria-label='GitHub' icon={<FaGithub />} />
+          </Link>
+        </ButtonGroup>
+      </Stack>
+    </Stack>
+  </Container>
+);

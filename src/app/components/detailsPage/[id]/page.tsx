ae4222c4b52a821144add9338dfe8fa8ac6e271d
agacia1805 <agacia1805@gmail.com>
2024-01-15 20:42:23 +0000

    (feat) create initial files for details page

---
+++ src/app/components/detailsPage/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { Container, IconButton, Stack, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Link } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export const DetailsPage = () => (
+  <Container
+    as='main'
+    role='contentinfo'
+    maxW='container.xxl'
+    py={{ base: '2', md: '4' }}
+    px={{ base: '12', md: '16' }}
+  >
+
+  </Container>
+);

eb0d7d0c8f95b712dda292e115cac3a15787e35a
agacia1805 <agacia1805@gmail.com>
2024-01-17 11:24:09 +0000

    (feat) add smaller fetch limit for mobile

--- src/app/hooks/useTrips.ts
+++ src/app/hooks/useTrips.ts
@@ -4,11 +4,13 @@ import {
   InfiniteData,
 } from '@tanstack/react-query';
 import { ITrip, IPaginatedTripsResponse } from '../types';
+import { useIsMobile } from '../utils/useIsMobile';
 
 const fetchTrips = async (
-  pageParam: number = 1
+  pageParam: number = 1,
+  limit: number = 9
 ): Promise<IPaginatedTripsResponse> => {
-  const response = await fetch(`/api/trips?page=${pageParam}&limit=9`);
+  const response = await fetch(`/api/trips?page=${pageParam}&limit=${limit}`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
@@ -19,11 +21,14 @@ export const useTrips = (): UseInfiniteQueryResult<
   InfiniteData<IPaginatedTripsResponse, unknown>,
   Error
 > => {
+  const isMobile = useIsMobile();
+  const limit: number = isMobile ? 4 : 9;
+
   return useInfiniteQuery<IPaginatedTripsResponse, Error>({
     queryKey: ['trips'],
     queryFn: ({ pageParam = 1 }) => {
       const pageNumber = typeof pageParam === 'number' ? pageParam : 1;
-      return fetchTrips(pageNumber);
+      return fetchTrips(pageNumber, limit);
     },
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.hasMore ? allPages.length + 1 : undefined;
